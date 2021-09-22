629f5a8800cf0152ddf906fb0ac02d314ab27b93
无语无语 <1139543453@qq.com>
2021-09-22 23:32:09 +0800

    add:配置基础路由

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react'
+// import { Button } from 'antd-mobile'
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Home from './pages/Home'
+import CityList from './pages/CityList'
 
 function App() {
   return (
-    <div className="App">
-      这是根组件
-    </div>
+    <Router>
+      <div className = "App">
+        <Link to = '/home'>首页</Link>
+        <Link to = '/citylist'>城市选择</Link>
+
+        <Route path = "/home" component = {Home}></Route>
+        <Route path = '/citylist' component = {CityList}></Route>
+      </div>
+    </Router>
   );
 }
 
